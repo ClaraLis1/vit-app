431077a28a1420e6484dbd4b7888a330f9aecc15
Your Name <lisleclara1@gmail.com>
2023-09-26 13:12:34 -0300

    font

--- src/components/ui/unitCards/UnitCards.tsx
+++ src/components/ui/unitCards/UnitCards.tsx
@@ -6,18 +6,10 @@ import styles from '@/components/ui/unitCards/UnitCards.module.css'
 
 const UnitCards = () => {
   return (
-      // <div  className={styles.separador}> 
-      //     <Image src='/logo-vita.png'  alt='logo' width={400} height={400} className={styles.image}></Image>
-      //   </div>
-      <div className={styles.imageContainer}>
-      <Image
-        src="/cebada.jpeg"
-        alt="Imagen"
-        width={800}
-        height={600}
-        className={styles.image}
-      />
-    </div>
+       <div  className={styles.separador}> 
+          <Image src='/logo-vita.png'  alt='logo' width={400} height={400} className={styles.image}></Image>
+        </div>
+     
 
   )
 }
