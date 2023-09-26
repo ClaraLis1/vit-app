48fcf577ab7e3a343ed10d023eb1cc1439927fad
Your Name <lisleclara1@gmail.com>
2023-09-26 08:22:56 -0300

    footer

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -11,11 +11,13 @@ export const Footer = () => {
         <div className={styles.infoContainer}>          
             <p>Rivadavia 2235 Olavarría, Bs. As. | Argentina </p>
             <p><Link  className={styles.contact} target="_blank" href="https://web.whatsapp.com/send?phone=5492494000857&text=Hola,%20necesito%20hacerles%20una%20consulta..." ><FontAwesomeIcon icon={faWhatsapp} className={styles.icon} />  +54 9 2494 000857</Link></p>
+            <p><Link  className={styles.contactMobile} target="_blank" href="https://wa.me/5492494000857?text=Hola,%20necesito%20hacerles%20una%20consulta..." ><FontAwesomeIcon icon={faWhatsapp} className={styles.icon} />  +54 9 2494 000857</Link></p>
             <p>info@vita.com.ar</p>
         </div>
         <div className={styles.infoContainerRight}>          
             <p>Ruta 226 Km 163.4 (Punto Tandil) Tandil, Bs. As.</p>
             <p><Link  className={styles.contact} target="_blank" href="https://web.whatsapp.com/send?phone=5492494000857&text=Hola,%20necesito%20hacerles%20una%20consulta..." ><FontAwesomeIcon icon={faWhatsapp} className={styles.icon} />  +54 9 2494 000857</Link></p>
+            <p><Link  className={styles.contactMobile} target="_blank" href="https://wa.me/5492494000857?text=Hola,%20necesito%20hacerles%20una%20consulta..." ><FontAwesomeIcon icon={faWhatsapp} className={styles.icon} />  +54 9 2494 000857</Link></p>
             <p>info@vita.com.ar</p>
         </div>
         <div>
