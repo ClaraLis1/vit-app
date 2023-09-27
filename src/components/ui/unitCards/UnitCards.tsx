398fa80dcbe3613a29c75b6a9afeb834fd31327b
Your Name <lisleclara1@gmail.com>
2023-09-27 10:57:45 -0300

    acopio

--- src/components/ui/unitCards/UnitCards.tsx
+++ src/components/ui/unitCards/UnitCards.tsx
@@ -6,9 +6,21 @@ import styles from '@/components/ui/unitCards/UnitCards.module.css'
 
 const UnitCards = () => {
   return (
-       <div  className={styles.separador}> 
-          <Image src='/logo-vita.png'  alt='logo' width={400} height={400} className={styles.image}></Image>
+      <div className={styles.container}>
+        <div className={styles.imageContainer}></div>
+        <div className={styles.textContainer}>
+          <h1>Acopio</h1>
+          <p>
+          Comprendemos la Importancia de la cosecha y nos dedicamos a brindar al productor la oportunidad de comercializar sus granos en diversos mercados y puertos, colaborando estrechamente con distintos exportadores para asegurar el mejor negocio posible.
+Trabajamos con molinos y consumos locales ampliando las opciones disponibles para el productor y garantizando un negocio más favorable.
+En Insumos ofrecemos una amplia gama de productos esenciales para respaldar el éxito de sus cultivos.
+
+          </p>
         </div>
+
+      </div>
+       
+
      
 
   )
