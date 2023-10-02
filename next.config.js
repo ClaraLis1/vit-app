f178ab302a73cfd15c9ef83c01853ee330cd861b
Your Name <lisleclara1@gmail.com>
2023-10-02 15:27:08 -0300

    fuente y json

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'page.ts', 'js', 'ts', 'tsx', 'route.tsx', 'route.ts', 'css']
+  pageExtensions: ['page.tsx', 'page.ts', 'js', 'ts', 'tsx', 'route.tsx', 'route.ts']
 }
 
 module.exports = nextConfig
